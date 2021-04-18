d141cebfacf230a17c09ff1f6d4ed6963b1be488
MingxiHuang <mh954@cornell.edu>
2021-04-18 00:00:28 -0400

    just followed what's in the tutorial video

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,20 +1,18 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "Go!News"
+net_id = "Simon Huang (mh954), Beining Yang(by258), Zhiqian Ma(zm79), Xirui He(xh358)"
+
 
 @irsystem.route('/', methods=['GET'])
 def search():
-	query = request.args.get('search')
-	if not query:
-		data = []
-		output_message = ''
-	else:
-		output_message = "Your search: " + query
-		data = range(5)
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
-
-
-
+    query = request.args.get('search')
+    if not query:
+        data = []
+        output_message = ''
+    else:
+        output_message = "Your search: " + query
+        data = "helloworld"
+    return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
