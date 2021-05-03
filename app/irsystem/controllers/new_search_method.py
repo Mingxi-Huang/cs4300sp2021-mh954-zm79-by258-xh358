56f4b969b930f7a5ce5b0347336b35935b2c6d98
MingxiHuang <mh954@cornell.edu>
2021-05-02 23:45:18 -0400

    update search to be tfidf cosine similarity search

--- app/irsystem/controllers/new_search_method.py
+++ app/irsystem/controllers/new_search_method.py
@@ -1,30 +1,104 @@
-# add import
-import os
-import json
-from nltk.tokenize import TreebankWordTokenizer
 from collections import defaultdict
+from collections import Counter
+import json
+import math
+import string
+import time
 import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-jsonPath = dir_path+'/../../../datasource/categorized_news.json'
-
-with open(jsonPath, "r") as f:
-    newsList = json.load(f)
-
-# print(len(newsList))
-# print(newsList[0])
+import os
+from nltk.tokenize import TreebankWordTokenizer
 
 
-tfidf_vec = TfidfVectorizer(
-    stop_words='english', max_df=0.8, min_df=10, max_features=5000)
-tfidf_mat = tfidf_vec.fit_transform([d['title'] for d in newsList]).toarray()
+treebank_tokenizer = TreebankWordTokenizer()
 
 
-def get_cos_sim(query, doc, tfidf_vec):
-    vec1 = tfidf_vec.fit_transform(query)
-    vec2 = tfidf_vec.fit_transform(doc)
-    dotprod = np.dot(vec1, vec2)
-    v1_norm = np.linalg.norm(vec1)
-    v2_norm = np.linalg.norm(vec2)
-    return dotprod / (v1_norm * v2_norm)
+def build_inverted_index(newsList):
+    n = len(newsList)
+    result = dict()
+    for i in range(n):
+        wc = dict()
+        for word in newsList[i]['tokens']:
+            wc[word] = wc.setdefault(word, 0)+1
+        for word, count in wc.items():
+            result.setdefault(word, []).append((i, count))
+    return result
+
+
+def boolean_search(query_word, excluded_word, inverted_index):
+    query_word = query_word.lower()
+    excluded_word = excluded_word.lower()
+    M = []
+    A, B = [], []
+    for doc_id, count in inverted_index[query_word]:
+        if count > 0:
+            A.append(doc_id)
+    for doc_id, count in inverted_index[excluded_word]:
+        if count > 0:
+            B.append(doc_id)
+    i, j = 0, 0
+    while i < len(A) and j < len(B):
+        if A[i] == B[j]:
+            i += 1
+            j += 1
+        else:
+            if A[i] < B[j]:
+                M.append(A[i])
+                i += 1
+            else:
+                j += 1
+    if i <= len(A) - 1:
+        for k in range(i, len(A)):
+            M.append(A[i])
+    return M
+
+
+def compute_idf(inv_idx, n_docs, min_df=15, max_df_ratio=0.90):
+    idf = dict()
+    for word, tuples in inv_idx.items():
+        df_word = len(tuples)
+        df_ratio = df_word / n_docs
+        if min_df > df_word or max_df_ratio < df_ratio:
+            continue
+        idf[word] = np.log2(n_docs / (1 + df_word))
+    return idf
+
+
+def compute_doc_norms(index, idf, n_docs):
+    norms = np.zeros(n_docs)
+    for word, idf_score in idf.items():
+        for doc_id, count in index[word]:
+            norms[doc_id] += (count * idf_score) ** 2
+    return np.sqrt(norms)
+
+
+def index_search(query, index, idf, doc_norms, tokenizer=treebank_tokenizer):
+    tok_q = tokenizer.tokenize(query.lower())
+
+    query_tf = dict()
+    for word in tok_q:
+        query_tf[word] = query_tf.setdefault(word, 0) + 1
+
+    index_dict = dict()
+    for word in index:
+        index_dict[word] = dict()
+        for doc_id, count in index[word]:
+            index_dict[word][doc_id] = count
+
+    result = []
+    for doc_id in range(len(doc_norms)):
+        qnorm = 0
+        dotprod = 0
+        for word in query_tf.keys():
+            if word not in idf:
+                continue
+            query_tfidf = query_tf[word] * idf[word]
+            doc_tfidf = index_dict[word].get(doc_id, 0) * idf[word]
+            qnorm += query_tfidf ** 2
+            dotprod += query_tfidf * doc_tfidf
+        qnorm = np.sqrt(qnorm)
+        denom = doc_norms[doc_id] * qnorm
+        score = (dotprod / denom) if denom != 0 else 0
+        result.append((score, doc_id))
+
+    result = sorted(result, key=lambda x: x[0], reverse=True)
+    return result
--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -4,7 +4,6 @@ from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 import os
 import re
-import random
 from collections import Counter
 import math
 
@@ -16,6 +15,7 @@ from collections import defaultdict
 from collections import Counter
 import numpy as np
 from app.irsystem.controllers.redditRequest import getRedditData, top_five, getRedditResult
+from app.irsystem.controllers.new_search_method import build_inverted_index, compute_idf, compute_doc_norms, index_search
 
 project_name = "Go!News"
 net_id = "Simon Huang (mh954), Beining Yang(by258), Zhiqian Ma(zm79), Xirui He(xh358)"
@@ -26,95 +26,50 @@ jsonPath = dir_path+'/../../../datasource/categorized_news.json'
 with open(jsonPath, "r") as f:
     newsList = json.load(f)
 
-
-def verbatim_search_on_title(query):
-    result = []
-    for news in newsList:
-        title = news["title"]
-        if query in title:
-            result.append(title)
-    if len(result) < 10:
-        return result
-    return random.sample(result, 10)
-
-# print(kvList)
+treebank_tokenizer = TreebankWordTokenizer()
+for news in newsList:
+    news['tokens'] = treebank_tokenizer.tokenize(news['title'].lower())
+inv_idx = build_inverted_index(newsList)
+idf = compute_idf(inv_idx, len(newsList),
+                  min_df=15,
+                  max_df_ratio=0.9)
+inv_idx = {key: val for key, val in inv_idx.items()
+           if key in idf}
+doc_norms = compute_doc_norms(inv_idx, idf, len(newsList))
 
 
-history = []
+# def get_cos_similarity(vec_text1, vec_text2):
+#     intersection = set(vec_text1.keys()) & set(vec_text2.keys())
+#     numerator = sum([vec_text1[x] * vec_text2[x] for x in intersection])
+#     sum1 = sum([vec_text1[x] ** 2 for x in list(vec_text1.keys())])
+#     sum2 = sum([vec_text2[x] ** 2 for x in list(vec_text2.keys())])
+#     denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+#     if not denominator:
+#         return 0.0
+#     else:
+#         return float(numerator) / denominator
 
 
-def hot_search(query):
-    if query not in history:
-        history.append(query)
+# def text_to_vector(text):
+#     words = WORD.findall(text)
+#     return Counter(words)
 
 
-def list_to_str(str_lst):
-    result = ""
-    index = 0
-    for string in str_lst:
-        result = result + (" " if index == 0 else ", ") + string
-        index += 1
-    return result
+# def sim_search(query):
+#     qvec = text_to_vector(query)
+#     result = []
+#     for news in newsList:
+#         title = news['title']
+#         tvec = text_to_vector(title)
+#         sim_measure = get_cos_similarity(qvec, tvec)
+#         news['sim'] = sim_measure
 
-
-# -------------------------------
-def tokenize(searchContent):
-    return [x for x in re.findall(r"[a-z]+", searchContent.lower())]
-
-# titleList = [x["title"] for x in news]
-
-
-WORD = re.compile(r"\w+")
-
-
-def get_cos_similarity(vec_text1, vec_text2):
-    intersection = set(vec_text1.keys()) & set(vec_text2.keys())
-    numerator = sum([vec_text1[x] * vec_text2[x] for x in intersection])
-    sum1 = sum([vec_text1[x] ** 2 for x in list(vec_text1.keys())])
-    sum2 = sum([vec_text2[x] ** 2 for x in list(vec_text2.keys())])
-    denominator = math.sqrt(sum1) * math.sqrt(sum2)
-
-    if not denominator:
-        return 0.0
-    else:
-        return float(numerator) / denominator
-
-
-def text_to_vector(text):
-    words = WORD.findall(text)
-    return Counter(words)
-
-
-# Xirui adds another method
-def word_to_vec_without_stopwords(text):
-    WORD = re.compile(r"\w+")
-    words = WORD.findall(text)
-    clean_words = []
-    for word in words:
-        if word not in stopwords.words('english'):
-            clean_words.append(word)
-    return Counter(clean_words)
-
-
-def get_first_sentence(content):
-    contentList = content.split('.' + ' ')
-    return contentList[0]
-
-
-def sim_search(query):
-    qvec = text_to_vector(query)
-    result = []
-    for news in newsList:
-        title = news['title']
-        tvec = text_to_vector(title)
-        sim_measure = get_cos_similarity(qvec, tvec)
-        news['sim'] = sim_measure
-        news['first_sent'] = get_first_sentence(news['content']) + ' ...'
-        result.append((news['title'], news['first_sent'],
-                      news['sim'], news['url']))
-    result = sorted(result, key=lambda x: x[2], reverse=True)
-    returnList = [(x[0], x[3], x[2]) for x in result]
-    return returnList[:10]
+#         result.append((news['title'],
+#                       news['sim'], news['url']))
+#     result = sorted(result, key=lambda x: x[1], reverse=True)
+#     returnList = [(x[0], x[2], x[1]) for x in result]
+#     return returnList[:10]
 
 
 def getComments(keyword):
@@ -138,12 +93,15 @@ def search():
             reddit_lst = getComments(word)
             for item in reddit_lst:
                 reddit.append(item)
-        #search_history = "Your search history:" + list_to_str(history)
-        news = sim_search(query)
+        newsRank = index_search(query, inv_idx, idf, doc_norms)
+        newsResult = []
+        for score, doc_id in newsRank[:10]:
+            newsResult.append(
+                (newsList[doc_id]['title'], newsList[doc_id]['url'], score))
 
         context = {
             'reddit': reddit,
-            'news': news
+            'news': newsResult
         }
-        #data = index_search(query)
+
     return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, context=context)
